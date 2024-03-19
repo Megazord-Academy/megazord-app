fdce268b1931f1ed586e8bb1ca6e5766d350aa78
Raphael <raphael_kitahara@hotmail.com>
2024-03-19 11:21:19 -0300

    commit inicial

---
+++ MegazordApp/MegazordApp/MegazordAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MegazordAppApp.swift
+//  MegazordApp
+//
+//  Created by Raphael Ferezin Kitahara on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MegazordAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
